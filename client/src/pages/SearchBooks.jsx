de914b661721351065fa256ac512d6a12d08ca16
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-11-08 17:16:34 -0600

    worked on search and saved books code... still doesnt function

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -7,10 +7,12 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
-
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import {searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+// import { set } from '../../../server/models/Book';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -20,6 +22,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -70,13 +73,15 @@ const SearchBooks = () => {
     if (!token) {
       return false;
     }
-
+// EDITED THIS PART/////////////////////////////////////////////////////////
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
+      console.log(data);
+    
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+    
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
